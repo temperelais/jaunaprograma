d654e796602d896fd6f7c7df42a1fc1e58aa33ac
temperelais <149486977+temperelais@users.noreply.github.com>
2024-03-20 14:03:50 +0200

    pievienota jauna lapa majaslapai

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/teksts")
+def te():
+    return render_template("teksts.html")
+
 if __name__=='__main__':
     app.run(port = 5000)
 
