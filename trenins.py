d118fb6797ef0bf63f8f8545911a37dd2f6d3fd9
temperelais <149486977+temperelais@users.noreply.github.com>
2024-03-26 13:06:36 +0200

    Update trenins.py

--- trenins.py
+++ trenins.py
@@ -1,5 +1,16 @@
+def summa(sk1, sk2):
+    skaitlis1=sk1
+    skaitlis2=sk2
+    print(skaitlis1+skaitlis2)
+
+summa(2341,200)
+
 print("ABCDFEGH")
+
 pirmais = "6"
+
 print(pirmais)
+
 vārds = "nē"
+
 print(pirmais + vārds)
